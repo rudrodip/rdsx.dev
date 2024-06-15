2bb9e7b0acb40850ffb292ad64f86c83de36994f
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-15 22:34:39 +0600

    default light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn(inter.variable, fontHeading.variable)}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <div className="flex flex-col min-h-screen">
             <div className="flex-grow">
               <div className="app">{children}</div>
