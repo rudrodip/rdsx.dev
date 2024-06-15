2365ba00dc05f27f576d03efd7be48df805a02de
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-15 18:29:39 +0600

    mdx content renderer

---
+++ src/components/mdx/mdx-content-renderer.tsx
@@ -0,0 +1,17 @@
+import * as runtime from 'react/jsx-runtime'
+import { mdxComponents } from './components'
+
+const useMDXComponent = (code: string) => {
+  const fn = new Function(code)
+  return fn({ ...runtime }).default
+}
+
+interface MDXProps {
+  code: string
+  components?: Record<string, React.ComponentType>
+}
+
+export const MDXContentRenderer = ({ code, components }: MDXProps) => {
+  const Component = useMDXComponent(code)
+  return <Component components={{ ...mdxComponents, ...components }} />
+}
