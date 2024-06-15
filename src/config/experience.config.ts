7d29b95d2a86c64bcf73d59ac65860633a96d8fa
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-15 18:30:39 +0600

    experience config

---
+++ src/config/experience.config.ts
@@ -0,0 +1,89 @@
+import type { Experience } from "@/types";
+
+export const experiencesConfig: Experience[] = [
+  {
+    "title": "Founder & CEO",
+    "employmentType": "Founder & CEO",
+    "company": {
+      "name": "Sonicrypt",
+      "url": "https://sonicrypt.rdsx.dev"
+    },
+    "location": {
+      "name": "On site"
+    },
+    "start": "April 2024",
+    "end": "Present",
+    "description": [
+      "Developed world's first sonification device for blockchain transactions",
+      "Developed a web faced product for the device",
+      "Developed the firmware for the device",
+      "Developed a mobile app for configuring the device",
+    ]
+   },
+   {
+    "title": "Founding Engineer",
+    "employmentType": "Founding Engineer",
+    "company": {
+      "name": "Leapflow",
+      "url": "https://leapflow.tech"
+    },
+    "location": {
+      "name": "Remote"
+    },
+    "start": "May 2024",
+    "end": "Present",
+    "description": [
+      "Developed the landing page of Leapflow",
+      "Currently working on the core product of Leapflow",
+    ]
+   },
+  {
+    title: "Fullstack developer",
+    employmentType: "Open Source contributor",
+    company: {
+      name: "LunCo",
+      url: "https://lunco.space",
+    },
+    location: { name: "Remote" },
+    start: "December, 2023",
+    end: "February, 2024",
+    description: [
+      "Currently adding more features to the web faced product of LunCo",
+      "Developed the official website of LunCo",
+    ],
+  },
+  {
+    title: "Robot Programmer",
+    employmentType: "Internship",
+    company: {
+      name: "Research Lab Bangladesh",
+      url: "https://www.facebook.com/ResearchLabCtg/",
+    },
+    location: { name: "Remote" },
+    start: "December, 2023",
+    end: "April, 2024",
+    description: [
+      "Developed robot firmwares with C++ & Python",
+      "Gained experience in Arduino IDE & PlatformIO"
+    ],
+  },
+  {
+    title: "Secretary of IT",
+    employmentType: "Full-time",
+    company: {
+      name: "Rajshahi College Science Club",
+      url: "https://rcsc.vercel.app",
+    },
+    location: {
+      name: "On site",
+      url: "https://rc.edu.bd",
+    },
+    start: "November, 2022",
+    end: "December, 2023",
+    description: [
+      "Managed the IT department of the club",
+      "Built the official website of the club",
+      "Supervised competitive programming under Rajshahi College National Science Fest 2023",
+    ],
+  },
+];
