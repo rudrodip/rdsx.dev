99c70be10360006b19816b7d1a88ea5a87b543d5
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-15 22:33:12 +0600

    added custom screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -77,6 +77,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      screens: {
+        tablet: "1110px"
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
