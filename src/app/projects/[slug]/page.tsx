41bfe85f231e2751fb018040bcf50c37120a732a
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-15 23:11:57 +0600

    updated param

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,16 +1,16 @@
 import { projects } from "#site/content";
 import { MDXContentRenderer } from "@/components/mdx/mdx-content-renderer";
 
-export default function ProjectPage({ params }: { params: { id: string} }) {
-  const id = params.id;
-  const project = projects.find((project) => project.slugAsParams === id);
+export default function ProjectPage({ params }: { params: { slug: string} }) {
+  const slug = params.slug;
+  const project = projects.find((project) => project.slugAsParams === slug);
   if (!project) {
     return <p>Project not found</p>
   }
 
   return (
     <section>
-      <p>{id}</p>
+      <p>{slug}</p>
       <MDXContentRenderer code={project.body} />
     </section>
   )
