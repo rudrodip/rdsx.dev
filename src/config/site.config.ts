51b02e7e496161a3010968f28095f91bb3b091d9
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-15 13:43:21 +0600

    site config

---
+++ src/config/site.config.ts
@@ -0,0 +1,40 @@
+import type { Site } from "@/types";
+
+export const siteConfig: Site = {
+  name: "rdsx.dev",
+  title: "Rudro Dip Sarker | rdsx.dev | I gaslight rocks 🪨 to think 🧠",
+  description: "Hi, I'm Rudro Dip Sarker (Sumit) a.k.a rds - I program computers. I'm the founder of Sonicrypt & Echocity, and one of the founding engineers of Leapflow. I also make electronic devices, robots, make videos, and write blogs. I don't have a niche, I'm a generalist. I work on web, mobile, desktop, server, embedded systems, IoT, blockchain, applied AI. I love physics, and computer science. And... I gaslight rocks 🪨 to think 🧠. (I watch anime too)",
+  keywords: [
+    "Rudro Dip Sarker",
+    "Rudro Dip Sarker Sumit",
+    "rds",
+    "rdsx",
+    "rds_agi",
+    "rdsx.dev",
+    "github.com/rudrodip",
+    "Rudro",
+    "programmer",
+    "developer",
+    "engineer",
+    "founder",
+    "Sonicrypt",
+    "Echocity",
+    "Leapflow",
+    "Volcane AI",
+    "x.com/rds_agi",
+    "robots",
+    "electronics",
+    "physics",
+    "computer science",
+  ],
+  siteUrl: "https://rdsx.dev",
+  creator: {
+    name: "Rudro Dip Sarker",
+    url: "https://rdsx.dev",
+  },
+  ogImage: "https://rdsx.dev/og.png",
+  links: {
+    x: "https://x.com/rds_agi",
+    github: "https://github.com/rudrodip",
+  }
+}
