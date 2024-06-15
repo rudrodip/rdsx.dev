a3605698c952eced45bda31f08c123000ccf1bed
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-15 13:43:17 +0600

    portfolio config

---
+++ src/config/portfolio.config.ts
@@ -0,0 +1,17 @@
+import type { Portfolio } from "@/types";
+
+export const portfolioConfig: Portfolio = {
+  name: "Rudro Dip Sarker",
+  tagline: "I gaslight rocks 🪨 to think 🧠",
+  bio: "Hey 👋 RDS here! I am a software developer and founder of Sonicrypt. I build web applications, mobile applications, dapps, backend servers, and embedded devices.",
+  links: {
+    x: "https://x.com/rds_agi",
+    github: "https://github.com/rudrodip",
+    linkedin: "https://linkedin.com/in/rudrodip",
+    mail: "mailto:official.rudrodipsarker@gmail.com",
+    youtube: "https://www.youtube.com/@rudrodipsarker",
+    discord: "https://discordapp.com/users/841126921886498817",
+    facebook: "https://facebook.com/enthusiast.math",
+    instagram: "https://instagram.com/rds_agi",
+  }
+}
