742c1541d9d5dc142712909f0325fa63afd770f3
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-16 10:19:58 +0600

    replace image with picture

--- src/components/project/project-card.tsx
+++ src/components/project/project-card.tsx
@@ -1,30 +1,35 @@
 "use client";
 
-import type { Project } from "@/types";
+import { projects } from "#velite";
 import { ExternalLink, StepForward } from "lucide-react";
-import Image from "next/image";
-import { Link } from 'next-view-transitions'
+import { Link } from "next-view-transitions";
+import Picture from "@/components/picture";
+import { z } from "velite";
 
 type ProjectCardProps = {
-  project: Project;
+  project: z.infer<typeof projects.schema>;
 };
 
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
     <div className="flex p-3 justify-between gap-2 rounded-2xl border border-b-8 overflow-hidden">
       <div className="space-y-2 w-full tablet:w-3/5">
-        <Link
-          href={`/projects/${project.slugAsParams}`}
-          className="inline-flex items-center gap-1 group/link"
-        >
-          <h1 className="text-xl font-semibold">{project.title}</h1>
-          <span className="-translate-x-1 opacity-0 group-hover/link:translate-x-0 group-hover/link:opacity-100 transition-all duration-100 ease-in-out">
-            <StepForward size={12} />
-          </span>
+        <Link href={`/projects/${project.slugAsParams}`} className="space-y-2">
+          <div className="inline-flex items-center gap-1 group/link">
+            <div className="flex items-center gap-2">
+              <h1 className="text-xl font-semibold">{project.title}</h1>
+              <span className="text-xs px-2 py-1 rounded bg-secondary">
+                {new Date(project.date).toDateString()}
+              </span>
+            </div>
+            <span className="-translate-x-1 opacity-0 group-hover/link:translate-x-0 group-hover/link:opacity-100 transition-all duration-100 ease-in-out">
+              <StepForward size={12} />
+            </span>
+          </div>
+          <p className="text-sm text-muted-foreground max-w-2xl">
+            {project.description}
+          </p>
         </Link>
-        <p className="text-sm text-muted-foreground max-w-2xl">
-          {project.description}
-        </p>
         <div className="flex items-center gap-2">
           {project.tags.map((tag) => (
             <p
@@ -51,13 +56,10 @@ export default function ProjectCard({ project }: ProjectCardProps) {
       </div>
       <div className="w-2/5 aspect-video overflow-hidden pebble rounded-xl hidden tablet:block">
         <Link href={`/projects/${project.slugAsParams}`}>
-          <Image
-            src={project.image.src}
+          <Picture
+            image={project.image}
+            imageDark={project.imageDark}
             alt={project.title}
-            width={200}
-            height={200}
-            blurDataURL={project.image.blurDataURL}
-            placeholder="blur"
             className="w-full h-full object-cover"
           />
         </Link>
