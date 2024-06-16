561723c2213dbaac148d482934dd7a516f5219b4
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-16 10:20:07 +0600

    updated

--- src/components/tab-section/project-section.tsx
+++ src/components/tab-section/project-section.tsx
@@ -2,9 +2,13 @@ import { projects } from "#site/content"
 import ProjectCard from "@/components/project/project-card"
 
 export default function Projects() {
+  const projectsSorted = projects.sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
+
   return (
     <div className="mt-10 space-y-6">
-      {projects.map((project, index) => (
+      {projectsSorted.map((project, index) => (
         <ProjectCard key={index} project={project} />
       ))}
     </div>
