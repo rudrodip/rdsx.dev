f4cad6df0686461a26723343b83ab8a295fdc857
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-17 09:18:48 +0600

    init icon map

---
+++ src/components/icon-map.tsx
@@ -0,0 +1,13 @@
+import { GitHubLogoIcon, TwitterLogoIcon, DiscordLogoIcon, LinkedInLogoIcon, InstagramLogoIcon, GlobeIcon, VideoIcon } from "@radix-ui/react-icons";
+import { Mail } from 'lucide-react'
+
+export const IconMap = {
+  github: <GitHubLogoIcon />,
+  twitter: <TwitterLogoIcon />,
+  discord: <DiscordLogoIcon />,
+  linkedin: <LinkedInLogoIcon />,
+  instagram: <InstagramLogoIcon />,
+  website: <GlobeIcon />,
+  youtube: <VideoIcon />,
+  mail: <Mail size={16} />
+}
