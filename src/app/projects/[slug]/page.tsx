107e55fb2dfc224df7317106ea0a1e7668b9d574
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-17 09:18:43 +0600

    updated layout

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -13,52 +13,43 @@ export default function ProjectPage({ params }: { params: { slug: string } }) {
   }
 
   return (
-    <section className="relative my-16">
-      <Link
-        href="/"
-        className="absolute top-0 left-0 -translate-x-[150%] p-3 group/back inline-flex items-center text-xs"
-      >
-        <ChevronLeft
-          size={16}
-          className="group-hover/back:-translate-x-1 transition-all duration-100 ease-in-out"
-        />
-        <span>Go back</span>
-      </Link>
-      <Picture
-        image={project.image}
-        imageDark={project.imageDark}
-        alt={project.title}
-        className="rounded-2xl border border-b-8"
-      />
-      <h1 className="head-text-md p-1 mt-6 mb-3">{project.title}</h1>
-      <p className="border-l-4 rounded-xl p-1 pl-3">{project.description}</p>
-      <div className="space-x-2 h-10 my-4">
-        {project.links.map((link) => (
-          <a
-            key={link.name}
-            href={link.url}
-            target="_blank"
-            className="p-2 text-xs inline-flex gap-1 items-center rounded-lg pebble"
-          >
-            <span>{link.name}</span>
-            <ExternalLink size={12} />
-          </a>
-        ))}
+    <main className="w-full lg:h-screen p-0 sm:p-5">
+      <div className="w-full h-full rounded-2xl sm:border flex flex-wrap justify-between lg:divide-x">
+        <div className="relative w-full lg:w-2/5 p-2 md:p-8">
+        <Link href="/" className="absolute top-2 left-1 md:top-8 md:left-2 group/back text-xs">
+          <ChevronLeft size={20} />
+        </Link>
+          <Picture
+            image={project.image}
+            imageDark={project.imageDark}
+            alt={project.title}
+            className="border border-b-8 rounded-xl"
+          />
+          <h1 className="head-text-md p-1 mt-6 mb-3">{project.title}</h1>
+          <p className="border-l-4 rounded-xl p-1 pl-3">
+            {project.description}
+          </p>
+          <div className="space-x-2 h-10 my-4">
+            {project.links.map((link) => (
+              <a
+                key={link.name}
+                href={link.url}
+                target="_blank"
+                className="p-2 text-xs inline-flex gap-1 items-center rounded-lg pebble"
+              >
+                <span>{link.name}</span>
+                <ExternalLink size={12} />
+              </a>
+            ))}
+          </div>
+        </div>
+        <div
+          id="tab-section"
+          className="w-full lg:h-full lg:w-3/5 p-2 md:p-8 lg:overflow-y-scroll"
+        >
+          <MDXContentRenderer code={project.body} />
+        </div>
       </div>
-      <MDXContentRenderer code={project.body} />
-      <div className="space-x-2 h-10 my-4">
-        {project.links.map((link) => (
-          <a
-            key={link.name}
-            href={link.url}
-            target="_blank"
-            className="p-2 text-xs inline-flex gap-1 items-center rounded-lg pebble"
-          >
-            <span>{link.name}</span>
-            <ExternalLink size={12} />
-          </a>
-        ))}
-      </div>
-    </section>
+    </main>
   );
 }
