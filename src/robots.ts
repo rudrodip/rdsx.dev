a8d910a308d5a00bc73685e0a9accd21b9fe1be7
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-18 02:17:32 +0600

    added robots config

---
+++ src/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+import { siteConfig } from './config/site.config'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${siteConfig.siteUrl}/sitemap.xml`,
+  }
+}
