f0c8d895c6565335bba15360335408c5b560bb74
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-18 11:29:10 +0600

    feed route rewrites to rss xml

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/feed',
+        destination: '/rss.xml',
+      },
+    ]
+  },
+};
 
 export default nextConfig;
