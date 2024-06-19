1671ea19972f18d580fb6928d90e1dcc40f65b5f
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-19 19:55:36 +0600

    fixed import path issue

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next'
-import { siteConfig } from './config/site.config'
+import { siteConfig } from '@/config/site.config'
  
 export default function robots(): MetadataRoute.Robots {
   return {
