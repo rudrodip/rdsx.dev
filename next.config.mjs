27f00903296f75d9eb1932e9cff5d4c14195e05d
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-20 18:55:33 +0600

    added csp

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,32 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+default-src * data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'; 
+script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; 
+script-src-elem * data: blob: 'unsafe-inline' 'unsafe-eval';
+connect-src * data: blob: 'unsafe-inline'; 
+img-src * data: blob: 'unsafe-inline'; 
+media-src * data: blob: 'unsafe-inline'; 
+frame-src * data: blob: ; 
+style-src * data: blob: 'unsafe-inline';
+font-src * data: blob: 'unsafe-inline';
+frame-ancestors * data: blob:;
+`
+ 
 const nextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/feed',
-  //       destination: '/rss.xml',
-  //     },
-  //   ]
-  // },
-};
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
+  },
+}
 
 export default nextConfig;
