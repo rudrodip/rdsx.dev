ef5bf6645b815777be5f654846b574749acb03ca
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-21 10:25:00 +0600

    removed unnecessary border

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -81,7 +81,7 @@ export default function ProjectPage({ params }: ProjectPageProps) {
   }
 
   return (
-    <main className="relative w-full lg:h-screen p-0 sm:p-5 border-4">
+    <main className="relative w-full lg:h-screen p-0 sm:p-5">
       <div className="w-full h-full rounded-2xl sm:border flex flex-wrap justify-between lg:divide-x">
         <div className="relative w-full lg:w-2/5 p-2 md:p-8">
           <div className="flex justify-between mb-2">
