81de4125e9fd404b93df5cdf04b4595441cead83
rudrodip <official.rudrodipsarker@gmail.com>
2024-06-22 13:21:17 +0600

    added view transition toggle

--- src/components/theme/theme-toggler.tsx
+++ src/components/theme/theme-toggler.tsx
@@ -5,16 +5,37 @@ import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
 export default function ThemeToggler() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  const SWITCH = () => {
+    switch (theme) {
+      case "light":
+        setTheme("dark");
+        break;
+      case "dark":
+        setTheme("light");
+        break;
+      default:
+        break;
+    }
+  }
+
+  const TOGGLE_THEME = () => {
+    //@ts-ignore
+    if (!document.startViewTransition) SWITCH()
+    
+    //@ts-ignore
+    document.startViewTransition(SWITCH)
+  };
 
   return (
     <Button variant="ghost" size="icon" className="rounded-full">
       <SunIcon
-        onClick={() => setTheme("dark")}
+        onClick={TOGGLE_THEME}
         className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
       />
       <MoonIcon
-        onClick={() => setTheme("light")}
+        onClick={TOGGLE_THEME}
         className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
       />
       <span className="sr-only">Toggle theme</span>
