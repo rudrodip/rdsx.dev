0614b2377511e3c2badbd2b1dbe419139395aa2e
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-06 17:41:29 +0600

    fix: onClick event on button

--- src/components/theme/theme-toggler.tsx
+++ src/components/theme/theme-toggler.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 export default function ThemeToggler() {
   const { theme, setTheme } = useTheme();
 
-  const SWITCH = () => {
+  const switchTheme = () => {
     switch (theme) {
       case "light":
         setTheme("dark");
@@ -18,26 +18,25 @@ export default function ThemeToggler() {
       default:
         break;
     }
-  }
+  };
 
-  const TOGGLE_THEME = () => {
+  const toggleTheme = () => {
     //@ts-ignore
-    if (!document.startViewTransition) SWITCH()
-    
+    if (!document.startViewTransition) switchTheme();
+
     //@ts-ignore
-    document.startViewTransition(SWITCH)
+    document.startViewTransition(switchTheme);
   };
 
   return (
-    <Button variant="ghost" size="icon" className="rounded-full">
-      <SunIcon
-        onClick={TOGGLE_THEME}
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-      />
-      <MoonIcon
-        onClick={TOGGLE_THEME}
-        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-      />
+    <Button
+      onClick={toggleTheme}
+      variant="ghost"
+      size="icon"
+      className="rounded-full"
+    >
+      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
