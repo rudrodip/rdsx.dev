d9fb8108e78e04f9c86596cfb7f68c5cbd01b413
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-14 23:24:33 +0600

    updated resume link

--- src/config/portfolio.config.ts
+++ src/config/portfolio.config.ts
@@ -4,7 +4,7 @@ export const portfolioConfig: Portfolio = {
   name: "Rudro Dip Sarker",
   tagline: "I gaslight rocks 🪨 to think 🧠",
   bio: "Hey 👋 RDS here! I am a software developer and founder of Sonicrypt. I build web applications, mobile applications, dapps, backend servers, and embedded devices.",
-  resume: "https://drive.google.com/file/d/19oS3nmPcGAZBRMx9nW3BP6aYykDrZuiy/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1YavWrcNf0kyCfofna2Q8hE6DNgAxzy0N/view?usp=sharing",
   links: {
     github: "https://github.com/rudrodip",
     twitter: "https://x.com/rds_agi",
