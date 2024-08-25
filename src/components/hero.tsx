d438537b74938f3ed69f90f8a019e967e321c31a
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-25 15:33:18 +0600

    updated

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,53 +10,46 @@ import { skillsConfig } from "@/config/skills.config";
 export default function Hero() {
   return (
     <section className="w-full flex flex-col lg:min-h-[calc(100vh-7rem)]">
-      <div className="flex-1">
-        <Link href="/">
-          <span className="font-mono text-sm underline">{siteConfig.name}</span>
-        </Link>
-        <div className="flex justify-between items-center mt-6">
-          <h1 className="head-text-sm">{portfolioConfig.name}</h1>
-          <div className="flex items-center gap-2">
-            <Button
-              size="icon"
-              variant="ghost"
-              className="rounded-full"
-              asChild
-            >
-              <Link href="/feed">
-                <Rss size={18} />
-                <span className="sr-only">rss feed</span>
-              </Link>
-            </Button>
-            <ThemeToggler />
-          </div>
+      <Link href="/">
+        <span className="font-mono text-sm underline">{siteConfig.name}</span>
+      </Link>
+      <div className="flex justify-between items-center mt-6">
+        <h1 className="head-text-sm">{portfolioConfig.name}</h1>
+        <div className="flex items-center gap-2">
+          <Button size="icon" variant="ghost" className="rounded-full" asChild>
+            <Link href="/feed">
+              <Rss size={18} />
+              <span className="sr-only">rss feed</span>
+            </Link>
+          </Button>
+          <ThemeToggler />
         </div>
-        <h3 className="mt-2 text-lg">
-          {portfolioConfig.tagline} <span className="sr-only">tagline</span>
-        </h3>
-        <p className="my-6 max-w-2xl text-foreground/80">
-          Hey 👋 RDS here! I&apos;m currently working as a software engineer at{" "}
-          <a
-            href="https://radicalhealth.in"
-            target="_blank"
-            className="text-foreground font-semibold hover:underline"
-          >
-            Radical Health
-          </a>
-          . I&apos;m also the founder of{" "}
-          <a
-            href="https://sonicrypt.rdsx.dev"
-            target="_blank"
-            className="text-foreground font-semibold hover:underline"
-          >
-            Sonicrypt
-          </a>{" "}
-          - world&apos;s first blockchain sonification device.
-          <span className="sr-only">bio</span>
-        </p>
-        <Socials />
       </div>
-      <div className="hidden md:flex flex-col text-sm space-y-2 rounded max-w-2xl text-foreground/70 my-5">
+      <h3 className="mt-2 text-lg">
+        {portfolioConfig.tagline} <span className="sr-only">tagline</span>
+      </h3>
+      <p className="my-6 max-w-2xl text-foreground/80">
+        Hey 👋 RDS here! I&apos;m currently working as a software engineer at{" "}
+        <a
+          href="https://radicalhealth.in"
+          target="_blank"
+          className="text-foreground font-semibold hover:underline"
+        >
+          Radical Health
+        </a>
+        . I&apos;m also the founder of{" "}
+        <a
+          href="https://sonicrypt.rdsx.dev"
+          target="_blank"
+          className="text-foreground font-semibold hover:underline"
+        >
+          Sonicrypt
+        </a>{" "}
+        - world&apos;s first blockchain sonification device.
+        <span className="sr-only">bio</span>
+      </p>
+      <Socials />
+      <div className="hidden md:flex flex-col text-sm space-y-2 rounded max-w-2xl text-foreground/70 my-7">
         {skillsConfig.map((skill) => (
           <p key={skill.category}>
             <span className="font-semibold text-primary/90">
