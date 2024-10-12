214dc09ae39547381454ae1c370a4dacc748f283
rudrodip <official.rudrodipsarker@gmail.com>
2024-10-12 08:31:45 +0600

    updated config

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -2,7 +2,7 @@ import type { Site } from "@/types";
 
 export const siteConfig: Site = {
   name: "rdsx.dev",
-  title: "Rudro Dip Sarker | rdsx.dev | I gaslight rocks 🪨 to think 🧠",
+  title: "Rudro Dip Sarker | I gaslight rocks 🪨 to think 🧠",
   description: "Hi, I'm Rudro Dip Sarker (Sumit) a.k.a rds - I program computers. I'm the founder of Sonicrypt, and one of the founding engineers of Leapflow. I also make electronic devices, robots, make videos, and write blogs. I don't have a niche, I'm a generalist. I work on web, mobile, desktop, server, embedded systems, IoT, blockchain, applied AI. I love physics, and computer science. And... I gaslight rocks 🪨 to think 🧠. (I watch anime too)",
   keywords: [
     "Rudro Dip Sarker",
