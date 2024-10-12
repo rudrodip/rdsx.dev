5785aee63e56b4df97529bfa4e383da6718353ee
rudrodip <official.rudrodipsarker@gmail.com>
2024-10-12 08:31:52 +0600

    updated styles

--- src/components/blog/blog-card.tsx
+++ src/components/blog/blog-card.tsx
@@ -10,22 +10,24 @@ export default function BlogCard({
 }) {
   return (
     <Link href={`/blogs/${blog.slugAsParams}`}>
-      <div className="w-full h-full max-w-lg rounded-xl overflow-hidden border hover:border-primary duration-100 transition-all ease-in-out">
-        <Picture
-          image={blog.image}
-          imageDark={blog.imageDark}
-          width={200}
-          height={111}
-          alt={blog.title}
-          className="w-full aspect-[8/5] object-cover"
-        />
+      <div className="w-full h-full max-w-lg rounded-xl overflow-hidden border group/blog-card bg-background hover:bg-muted/50 duration-100 transition-all ease-in-out">
+        <div className="w-full aspect-[8/5] overflow-hidden">
+          <Picture
+            image={blog.image}
+            imageDark={blog.imageDark}
+            width={200}
+            height={111}
+            alt={blog.title}
+            className="w-full aspect-[8/5] object-cover scale-105 group-hover/blog-card:scale-100 duration-300 transition-all ease-in-out"
+          />
+        </div>
         <div className="p-3">
           <h1 className="text-xl font-semibold font-heading">{blog.title}</h1>
           <div className="flex items-center gap-2 flex-wrap mt-2">
             {blog.tags.map((tag) => (
               <p
                 key={tag}
-                className="px-2 py-1 rounded-lg bg-muted text-xs cursor-pointer"
+                className="px-2 py-1 rounded-lg bg-muted text-muted-foreground text-xs cursor-pointer"
               >
                 {tag}
               </p>
@@ -35,10 +37,13 @@ export default function BlogCard({
             {blog.description}
           </p>
           <div className="w-full flex justify-end">
-
-          <p className="text-xs text-muted-foreground mt-2 px-2 py-1 rounded bg-secondary">
-            {new Date(blog.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
-          </p>
+            <p className="text-xs mt-2 px-2 py-1 rounded bg-secondary">
+              {new Date(blog.date).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
           </div>
         </div>
       </div>
